8ac084968a6d3c9e69ebbb32238bcf7996897171
anggi <ortegacanillo76@gmail.com>
2024-07-26 20:35:49 +0800

    added posts routes and controllers

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -3,48 +3,54 @@ import { Post } from "../models/post.model.js";
 import path from "path";
 import fs from "fs/promises";
 import { httpError } from "../helpers/httpError.js";
+import { cloudinary } from "../helpers/cloudinaryUpload.js";
 
 const createPost = async (req, res, next) => {
   const { _id } = req.user;
   const { title, description, section } = req.body;
   const { path: tempPath, originalname } = req.file;
-
   const { error } = createPostValidation.validate(req.body);
+
   if (error) {
-    throw httpError(400, error.message);
+    return next(httpError(400, error.message));
   }
 
-  const extension = path.extname(originalname);
+  try {
+    const extension = path.extname(originalname);
+    const filename = `${_id}${extension}`;
+    const newPath = path.join("public", "posts", "uploads", filename);
+    await fs.rename(tempPath, newPath);
 
-  const filename = `${_id}${extension}`;
-  const newPath = path.join("public", "posts", "uploads", filename);
-  await fs.rename(tempPath, newPath);
+    const uploadedResponse = await cloudinary.uploader.upload(newPath, {
+      folder: "posts",
+      format: "jpg",
+    });
 
-  let contentURL = path.join("/posts", "uploads", filename);
-  contentURL = contentURL.replace(/\\/g, "/");
+    const post = await Post.create({
+      title,
+      description,
+      content: uploadedResponse.secure_url,
+      section,
+    });
 
-  const post = await Post.create({
-    title,
-    description,
-    content: contentURL,
-    section,
-  });
-
-  return res.status(201).json({ post });
+    return res.status(201).json({ post });
+  } catch (error) {
+    return next(httpError(500, error.message));
+  }
 };
 
 const getPosts = async (_req, res) => {
-  const posts = await Post.find();
+  const posts = await Post.find().populate("section");
 
   return res.json({ posts });
 };
 
 const getPostById = async (req, res) => {
   const { id } = req.params;
-  const post = await Post.findById(id);
+  const post = await Post.findById(id).populate("section");
 
   if (!post) {
-    throw httpError(404, "Not found");
+    return res.status(404).json({ message: "Not found" });
   }
 
   return res.json({ post });
@@ -52,37 +58,15 @@ const getPostById = async (req, res) => {
 
 const updatePost = async (req, res) => {
   const { id } = req.params;
-  const { _id } = req.user;
-  const { title, description, section } = req.body;
-  const { path: tempPath, originalname } = req.file;
-
-  const { error } = createPostValidation.validate(req.body);
-  if (error) {
-    throw httpError(400, error.message);
-  }
-
-  const extension = path.extname(originalname);
-
-  const filename = `${_id}${extension}`;
-  const newPath = path.join("public", "posts", "uploads", filename);
-  await fs.rename(tempPath, newPath);
-
-  let contentURL = path.join("/posts", "uploads", filename);
-  contentURL = contentURL.replace(/\\/g, "/");
-
+  const { title, description, content, section } = req.body;
   const post = await Post.findByIdAndUpdate(
     id,
-    {
-      title,
-      description,
-      content: contentURL,
-      section,
-    },
+    { title, description, content, section },
     { new: true }
   );
 
   if (!post) {
-    throw httpError(404, "Not found");
+    return res.status(404).json({ message: "Not found" });
   }
 
   return res.json({ post });
@@ -93,7 +77,7 @@ const deletePost = async (req, res) => {
   const post = await Post.findByIdAndDelete(id);
 
   if (!post) {
-    throw httpError(404, "Not found");
+    return res.status(404).json({ message: "Not found" });
   }
 
   return res.json({ post });
---
+++ helpers/cloudinaryUpload.js
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from "cloudinary";
+
+const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_API_SECRET } = process.env;
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_API_SECRET,
+});
+
+export { cloudinary };
